5b7d9216185af06cd662a0d65102691440566c0a
Adrian Iacuone <aiacuone@gmail.com>
2024-09-13 13:55:58 +1000

    added pixijs importing pokemon images tailwind basic slot functionality

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import { onMount, setContext } from 'svelte';
+	import '../app.css';
+	import { writable, type Writable } from 'svelte/store';
+</script>
+
+<div class="w-full h-[100dvh]">
+	<slot />
+</div>
